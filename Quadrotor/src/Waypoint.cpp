7b958573958770d460deaffed3bdfcb0f2d279b0
Chris Good <goodca91@gmail.com>
2012-12-13 14:21:46 -0500

    Waypoint initial commit

---
+++ Quadrotor/src/Waypoint.cpp
@@ -0,0 +1,18 @@
+/*
+ * Waypoint.cpp
+ *
+ *  Created on: Dec 13, 2012
+ *      Author: chris
+ */
+
+#include "Waypoint.h"
+
+Waypoint::Waypoint() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Waypoint::~Waypoint() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Quadrotor/src/Waypoint.h
@@ -0,0 +1,17 @@
+/*
+ * Waypoint.h
+ *
+ *  Created on: Dec 13, 2012
+ *      Author: chris
+ */
+
+#ifndef WAYPOINT_H_
+#define WAYPOINT_H_
+
+class Waypoint {
+public:
+	Waypoint();
+	virtual ~Waypoint();
+};
+
+#endif /* WAYPOINT_H_ */
