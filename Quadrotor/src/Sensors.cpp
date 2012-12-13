536e304c4869089db8a7640b1c39805c315db01c
Chris Good <goodca91@gmail.com>
2012-12-13 14:20:03 -0500

    Sensors initial commit

---
+++ Quadrotor/src/Sensors.cpp
@@ -0,0 +1,18 @@
+/*
+ * Sensors.cpp
+ *
+ *  Created on: Dec 13, 2012
+ *      Author: chris
+ */
+
+#include "Sensors.h"
+
+Sensors::Sensors() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Sensors::~Sensors() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Quadrotor/src/Sensors.h
@@ -0,0 +1,17 @@
+/*
+ * Sensors.h
+ *
+ *  Created on: Dec 13, 2012
+ *      Author: chris
+ */
+
+#ifndef SENSORS_H_
+#define SENSORS_H_
+
+class Sensors {
+public:
+	Sensors();
+	virtual ~Sensors();
+};
+
+#endif /* SENSORS_H_ */
