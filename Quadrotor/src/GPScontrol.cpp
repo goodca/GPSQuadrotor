4814e06ccede44969cfabfdee92d3d7fb8941146
Chris Good <goodca91@gmail.com>
2012-12-13 14:43:49 -0500

    GPScontrol initial commit

---
+++ Quadrotor/src/GPScontrol.cpp
@@ -0,0 +1,18 @@
+/*
+ * GPScontrol.cpp
+ *
+ *  Created on: Dec 13, 2012
+ *      Author: chris
+ */
+
+#include "GPScontrol.h"
+
+GPScontrol::GPScontrol() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GPScontrol::~GPScontrol() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Quadrotor/src/GPScontrol.h
@@ -0,0 +1,17 @@
+/*
+ * GPScontrol.h
+ *
+ *  Created on: Dec 13, 2012
+ *      Author: chris
+ */
+
+#ifndef GPSCONTROL_H_
+#define GPSCONTROL_H_
+
+class GPScontrol {
+public:
+	GPScontrol();
+	virtual ~GPScontrol();
+};
+
+#endif /* GPSCONTROL_H_ */
