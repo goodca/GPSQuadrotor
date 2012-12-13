d59a18998b8a93bd4b63377d84390f15d26105f9
Chris Good <goodca91@gmail.com>
2012-12-13 14:16:39 -0500

    initial commit from eclipse

---
+++ Quadrotor/Debug/src/Quadrotor.d
@@ -0,0 +1,1 @@
+src/Quadrotor.d: ../src/Quadrotor.cpp
---
+++ Quadrotor/src/Quadrotor.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Quadrotor.cpp
+// Author      : Chris Good and David Cooper
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints
+	return 0;
+}
