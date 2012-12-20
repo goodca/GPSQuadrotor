5d036aff525845d8dfc539f2d92f3449f4bb56ec
Chris Good <goodca91@gmail.com>
2012-12-20 13:40:05 -0500

    initial gyroscope class

---
+++ Quadrotor/src/Gyroscope.cpp
@@ -0,0 +1,27 @@
+/*
+ * Gyroscope.cpp
+ *
+ *  Created on: Dec 20, 2012
+ *      Author: chris
+ */
+
+#include "Gyroscope.h"
+
+namespace std {
+
+void Gyroscope::update() {
+	// TODO Auto-generated constructor stub
+	int x;
+}
+
+Gyroscope::Gyroscope() {
+	// TODO Auto-generated constructor stub
+	update();
+}
+
+Gyroscope::~Gyroscope() {
+	// TODO Auto-generated destructor stub
+}
+
+
+} /* namespace std */
