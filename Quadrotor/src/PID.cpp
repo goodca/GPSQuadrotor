445cd0a5e0ef0d1bd5d67e5cbb27888567bd5e63
cooperdl <cooperdl@rose-hulman.edu>
2013-01-13 21:32:07 -0500

    added PID to quadrotor project

---
+++ Quadrotor/src/PID.cpp
@@ -0,0 +1,58 @@
+/*
+ * PID.cpp
+ *
+ *  Created on: Dec 20, 2012
+ *      Author: cooperdl
+ */
+#include <stdio.h>
+#include "PID.h"
+#include "consants.h"
+#include <sys/time.h>
+
+
+PID::PID(double setPoint0, double actualValue0, double previousError0,
+		double integral0, double output0, timeval timestart0,double Kp, double Ki, double Kd) {
+
+	this->setPoint = setPoint0;
+	this->actualValue = actualValue0;
+	this->previousError = previousError0;
+	this->integral = integral0;
+	this->output = output0;
+	this->timestart = timestart0;
+	this->Kp=Kp;
+	this->Ki=Ki;
+	this->Kd=Kd;
+
+}
+
+double PID::UpdateOutput() {
+
+	double error = setPoint - actualValue; //sets error
+	timeval end;
+	gettimeofday(&end, NULL);
+	double timechange = (1000000*end.tv_sec+end.tv_usec)-(1000000*timestart.tv_sec+timestart.tv_usec); //calculates the time interval
+	timestart.tv_sec = end.tv_sec;
+	timestart.tv_usec = end.tv_usec;//sets the start time for the next cycle to the end time of this cycle
+	integral = integral + error * timechange; //calculates the integral amount
+	double derivitive = (error - previousError) / timechange; //calculates the derivative
+	output = Kp * error + Ki * integral + Kd * derivitive; //adds up everything to create the output
+	previousError = error; //sets the error for the next cycle
+	return output;
+
+}
+
+void PID::UpdateActualValue(double actualValue0) {
+	this->actualValue = actualValue0;//used for feed back
+
+}
+
+void PID::UpdateSetPoint(double setPoint0) {
+	this->setPoint = setPoint0;//value you need to reach
+
+}
+
+PID::~PID() {
+
+
+
+}
---
+++ Quadrotor/src/PID.h
@@ -0,0 +1,39 @@
+/*
+ * PID.h
+ *
+ *  Created on: Dec 20, 2012
+ *      Author: cooperdl
+ */
+
+#ifndef PID_H_
+#define PID_H_
+#include <sys/time.h>
+
+class PID {
+private:
+	double setPoint;
+	double actualValue;
+	double previousError;
+	double integral;
+	double output;
+	double Kp;
+	double Ki;
+	double Kd;
+	timeval timestart;
+
+
+public:
+	PID(double setPoint0, double actualValue0, double previousError0,
+			double integral0, double output0, timeval timestart, double Kp, double Ki, double Kd);
+
+	double UpdateOutput();
+
+	void UpdateActualValue(double actualValue0);
+
+	void UpdateSetPoint(double setPoint0);
+
+	virtual ~PID();
+};
+
+
+#endif /* PID_H_ */
